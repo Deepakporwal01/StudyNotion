e56cc666e8e3fe11b00d12b3ccd7b6aec1ccd4b2
Deepak Porwal <132838666+Deepakporwal01@users.noreply.github.com>
2026-02-15 18:23:34 +0530

    Clean up Payments.js by removing commented code
    Removed commented-out code for clarity.

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -1,7 +1,7 @@
 // // Import the required modules
 // const express = require("express")
 // const router = express.Router()
-
+ 
 // const { capturePayment, verifySignature,sendPaymentSuccessEmail } = require("../controllers/Payments")
 // const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
 // router.post("/capturePayment", auth, isStudent, capturePayment)
